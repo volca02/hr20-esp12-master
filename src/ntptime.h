8a4836d57480983a2efd6bc9fe60f33643d89466
Filip Volejnik <f.volejnik@gmail.com>
2018-06-23 15:41:55 +0200

    use now() instead of ntp timer for now - testing sync packet handling and data decoding

--- src/ntptime.h
+++ src/ntptime.h
@@ -41,21 +41,28 @@ struct NTPTime {
     }
 
     time_t localTime() {
-        TimeChangeRule *tcr; // pointer to the time change rule, use to get TZ abbrev
+/*        TimeChangeRule *tcr; // pointer to the time change rule, use to get TZ abbrev
         time_t utc = timeClient.getEpochTime();
-        return  tz.toLocal(utc, &tcr);
+        return  tz.toLocal(utc, &tcr);*/
+        return now();
     }
 
     void printRTC(){
+        time_t local = localTime();
+
+/*
         static char sDate[9];
         static char sTime[11];
-        time_t local = localTime();
         int mil = timeClient.getMillis();
 
         sprintf(sDate, "Y%02x%02x%02x", year(local)-2000, month(local), day(local));
         sprintf(sTime, "H%02x%02x%02x%02x", hour(local), minute(local), second(local), mil/10);
 
-        Serial.printf("RTC: %s %s\n",sDate, sTime);
+        Serial.printf("RTC: %s %s\n",sDate, sTime);*/
+
+        Serial.printf("RTC: %02d.%02d.%02d %02d:%02d:%02d\n",
+                      day(local), month(local), year(local)-2000,
+                      hour(local), minute(local), second(local));
     }
 };
 
